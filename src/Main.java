3230313f7a2a23d3cdefea722a6348b4857c1fe1
northinrtm <artsdurden@gmail.com>
2022-08-17 20:43:21 +0300

    2 задание

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,12 @@ public class Main {
         String lastName = "Ivanov";
         String fullName = "Ivanov Ivan Ivanovich";
         System.out.println("ФИО сотрудника — " + fullName);
+
         System.out.println("\nЗадание 2:");
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("\nЗадание 3:");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName.replace('ё', 'e'));
     }
 }
