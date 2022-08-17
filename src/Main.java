1e44f40807b40b890cf7efdc159e124a8fef2272
northinrtm <artsdurden@gmail.com>
2022-08-17 20:35:22 +0300

    1 задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Задание 1:");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 }
